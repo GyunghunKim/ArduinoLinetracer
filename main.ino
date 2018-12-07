d76e756cce4aeebfff54b30cd20c8fc06595923e
gyunghunkim <ghuni@snu.ac.kr>
2018-12-07 11:16:36 +0900

    직진하는 것 작성, 사거리를 만나거나 코너를 만난 경우에 대해 작성 필요 / 직진 테스트 필요

--- main.ino
+++ main.ino
@@ -1,8 +1,12 @@
 /*
  * 2018년도 2학기 창의공학설계 미니프로젝트 2
  * 2018. 12. 07
- * 김경훈, 김범석
+ * 20조
 */
+
+//핀과 관련된 상수
+#define TRIGGER_PIN 12
+#define ECHO_PIN 11
 
 #define ENA 6
 #define IN1 7
@@ -11,19 +15,39 @@
 #define IN3 4
 #define IN4 2
 
+//바퀴를 나타내는 상수
 #define L_WHEEL 0x01
 #define R_WHEEL 0x10
 
+//방향과 관련된 상수
 #define STOP 0
 #define FOWARD 1
 #define BACK 2
+#define LEFT 3
+#define RIGHT 4
 
+//센서 상태와 관련된 상수
 #define SENSOR_THRESHOLD 200
 #define SENSOR_L 0x001
 #define SENSOR_R 0x010
 #define SENSOR_F 0x100
 
+//거리측정과 관련된 상수
+#define DISTANCE_THRESHOLD 15
+#define DISTANCE_COUNT_THRESHOLD 5
+
+//속도와 관련된 상수
+#define SPEED 150
+#define TURNSPEED 150
+
+//시간과 관련된 상수
+#define TICK 75
+#define TOCK 10
+#define TURNTIME 200
+
 int sensorStatus = 0;
+int distance = 0;
+int distanceCount = 0;
 
 void wheel(int lr, int dir, int speed) {
 	if (lr & L_WHEEL) {
@@ -66,37 +90,99 @@ void sense() {
 	sensorStatus = ((analogRead(MODULE_L) > SENSOR_THRESHOLD) ? SENSOR_L : 0) | 
 				((analogRead(MODULE_R) > SENSOR_THRESHOLD) ? SENSOR_R : 0) | 
 				((analogRead(MODULE_F) > SENSOR_THRESHOLD) ? SENSOR_F : 0);
+	
+	digitalWrite(TRIGGER_PIN, HIGH);
+	delayMicroseconds(10);
+	digitalWrite(TRIGGER_PIN, LOW);
+	distance = pulseIn(ECHO_PIN, HIGH)/58;	
+
+	delay(10);
 }
 
-int turnRight() {
+void roll(int dir, int speed, int time) {
+	if (dir == FOWARD)
+		wheel(L_WHEEL | R_WHEEL, FOWARD, speed);
+	else if (dir == BACK)
+		wheel(L_WHEEL | R_WHEEL, BACK, speed);
+	else if (dir == RIGHT) {
+		wheel(L_WHEEL, FOWARD, speed);
+		wheel(R_WHEEL, BACK, speed);
+	}
+	else if (dir == LEFT) {
+		wheel(L_WHEEL, BACK, speed);
+		wheel(R_WHEEL, FOWARD, speed);
+	}
+	delay(time);
+	wheel(L_WHEEL | R_WHEEL, stop, 0);
+}
+
+void turnRight() {
 
 }
 
-int turnLeft() {
+void turnLeft() {
 
 }
 
-int turnBack() {
+void turnAround() {
+	roll(RIGHT, TURNSPEED, TURNTIME);
 
+	do {
+		sense()
+		roll(RIGHT, TURNSPEED, TICK);
+	} while (sensorStatus != SENSOR_F);
 }
 
 void setup() {
+	pinMode(TRIGGER_PIN, OUTPUT);
+	pinMode(ECHO_PIN, INPUT);
+
 	pinMode(ENA, OUTPUT);
 	pinMode(IN1, OUTPUT);
 	pinMode(IN2, OUTPUT);
 	pinMode(ENB, OUTPUT);
 	pinMode(IN3, OUTPUT);
 	pinMode(IN4, OUTPUT);
+
+	wheel(L_WHEEL | R_WHEEL, STOP, 0);
 }
 
 void loop() {
 	//감지 --> 판단 --> 움직임
 	sense();
-	delay(10);
 
+	//앞에 장애물이 있는지 체크하고
+	if (distance < DISTANCE_THRESHOLD) {
+		distanceCount++;
+	} else {
+		ditanceCount = 0;
+	}
+	//장애물이 있다면(THRESHOLD회 이상 있는 것으로 판단된 경우)뒤로 돈다.
+	if (distanceCount < DISTANCE_COUNT_THRESHOLD) {
+		distanceCount = 0;
+		turnAround();
 	
+		return;
+	}
 
+	//사거리를 만난 경우
+	if (sensorStatus & SENSOR_F && sensorStatus & SENSOR_R && sensorStatus & SENSOR_L) {
+		turnRight();
+
+		return;
+	}
+
+	//직진
+	if (sensorStatus & SENSOR_L) {
+		roll(RIGHT, SPEED, TICK);
+		delay(TOCK);
+	} else if (sensorStatus & SENSOR_R) {
+		roll(LEFT, SPEED, TICK);
+		delay(TOCK);
+	} else {
+		roll(FRONT, SPEED, TICK);
+		delay(TOCK);
+	}
 	//모든 작업이 끝난 후 약간 멈추기
-	wheel(L_WHEEL | R_WHEEL, STOP, 0);
-	delay(10);	
+	delay(0);	
 }
