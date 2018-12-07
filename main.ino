96cd6c6f42e333b73707040deb48873b073e9a3b
Gyunghun Kim <ghuni@snu.ac.kr>
2018-12-08 00:06:06 +0900

    1차 완성본

--- main.ino
+++ main.ino
@@ -15,6 +15,10 @@
 #define IN3 4
 #define IN4 2
 
+#define MODULE_L A2
+#define MODULE_F A1
+#define MODULE_R A0
+
 //바퀴를 나타내는 상수
 #define L_WHEEL 0x01
 #define R_WHEEL 0x10
@@ -33,156 +37,229 @@
 #define SENSOR_F 0x100
 
 //거리측정과 관련된 상수
-#define DISTANCE_THRESHOLD 15
-#define DISTANCE_COUNT_THRESHOLD 5
+#define DISTANCE_THRESHOLD 8
+#define DISTANCE_COUNT_THRESHOLD 3
 
 //속도와 관련된 상수
-#define SPEED 150
-#define TURNSPEED 150
+#define SPEED 100
+#define TURNSPEED 200
 
 //시간과 관련된 상수
 #define TICK 75
-#define TOCK 10
-#define TURNTIME 200
+#define TOCK 50
+
+#define TURNTIME 100//75
+
+#define GO_STRAIGHT_BEFORE_TURN 3 
+
 
 int sensorStatus = 0;
 int distance = 0;
 int distanceCount = 0;
 
 void wheel(int lr, int dir, int speed) {
-	if (lr & L_WHEEL) {
-		if (dir == STOP) {
-			digitalWrite(IN1, LOW);
-			digitalWrite(IN2, LOW);
-			analogWrite(ENA, 0);	
-		}	
-		if (dir == FOWARD) {
-			digitalWrite(IN1, LOW);
-			digitalWrite(IN2, HIGH);
-			analogWrite(ENA, speed);
-		}
-		if (dir == BACK) {
-			digitalWrite(IN1, HIGH;
-			digitalWrite(IN2, LOW);
-			analogWrite(ENA, speed);
-		}
-	}
-	if (lr & R_WHEEL) {
-		if (dir == STOP) {
-			digitalWrite(IN3, LOW);
-			digitalWrite(IN4, LOW);
-			analogWrite(ENB, 0);	
-		}	
-		if (dir == FOWARD) {
-			digitalWrite(IN3, LOW);
-			digitalWrite(IN4, HIGH);
-			analogWrite(ENB, speed);
-		}
-		if (dir == BACK) {
-			digitalWrite(IN3, HIGH;
-			digitalWrite(IN4, LOW);
-			analogWrite(ENB, speed);
-		}
-	}
+  if (lr & R_WHEEL) {
+    if (dir == STOP) {
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, LOW);
+      analogWrite(ENA, 0);  
+    } 
+    if (dir == FOWARD) {
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, HIGH);
+      analogWrite(ENA, speed);
+    }
+    if (dir == BACK) {
+      digitalWrite(IN1, HIGH);
+      digitalWrite(IN2, LOW);
+      analogWrite(ENA, speed);
+    }
+  }
+  if (lr & L_WHEEL) {
+    if (dir == STOP) {
+      digitalWrite(IN3, LOW);
+      digitalWrite(IN4, LOW);
+      analogWrite(ENB, 0);  
+    } 
+    if (dir == FOWARD) {
+      digitalWrite(IN3, LOW);
+      digitalWrite(IN4, HIGH);
+      analogWrite(ENB, speed);
+    }
+    if (dir == BACK) {
+      digitalWrite(IN3, HIGH);
+      digitalWrite(IN4, LOW);
+      analogWrite(ENB, speed);
+    }
+  }
 }
 
 void sense() {
-	sensorStatus = ((analogRead(MODULE_L) > SENSOR_THRESHOLD) ? SENSOR_L : 0) | 
-				((analogRead(MODULE_R) > SENSOR_THRESHOLD) ? SENSOR_R : 0) | 
-				((analogRead(MODULE_F) > SENSOR_THRESHOLD) ? SENSOR_F : 0);
-	
-	digitalWrite(TRIGGER_PIN, HIGH);
-	delayMicroseconds(10);
-	digitalWrite(TRIGGER_PIN, LOW);
-	distance = pulseIn(ECHO_PIN, HIGH)/58;	
+  sensorStatus = ((analogRead(MODULE_L) > SENSOR_THRESHOLD) ? SENSOR_L : 0) | 
+        ((analogRead(MODULE_R) > SENSOR_THRESHOLD) ? SENSOR_R : 0) | 
+        ((analogRead(MODULE_F) > SENSOR_THRESHOLD) ? SENSOR_F : 0);
+  
+  digitalWrite(TRIGGER_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGGER_PIN, LOW);
+  distance = pulseIn(ECHO_PIN, HIGH)/58;  
 
-	delay(10);
+  delay(10);
 }
 
 void roll(int dir, int speed, int time) {
-	if (dir == FOWARD)
-		wheel(L_WHEEL | R_WHEEL, FOWARD, speed);
-	else if (dir == BACK)
-		wheel(L_WHEEL | R_WHEEL, BACK, speed);
-	else if (dir == RIGHT) {
-		wheel(L_WHEEL, FOWARD, speed);
-		wheel(R_WHEEL, BACK, speed);
-	}
-	else if (dir == LEFT) {
-		wheel(L_WHEEL, BACK, speed);
-		wheel(R_WHEEL, FOWARD, speed);
-	}
-	delay(time);
-	wheel(L_WHEEL | R_WHEEL, stop, 0);
+  if (dir == FOWARD)
+    wheel(L_WHEEL | R_WHEEL, FOWARD, speed);
+  else if (dir == BACK)
+    wheel(L_WHEEL | R_WHEEL, BACK, speed);
+  else if (dir == RIGHT) {
+    wheel(L_WHEEL, FOWARD, speed);
+    wheel(R_WHEEL, BACK, speed);
+  }
+  else if (dir == LEFT) {
+    wheel(L_WHEEL, BACK, speed);
+    wheel(R_WHEEL, FOWARD, speed);
+  }
+  delay(time);
+  
+  wheel(L_WHEEL | R_WHEEL, STOP, 0);
+}
+
+//직진하지만 길이 끝인 경우 dir 방향으로 움직인다.
+void goStraight(int dir) {
+    while(1) {
+    sense();
+    roll(FOWARD, SPEED, TICK);
+    delay(TOCK);
+    
+    if (sensorStatus == SENSOR_F)
+      break;
+    if ((sensorStatus & SENSOR_F) == 0) {
+      if (dir == RIGHT)
+        turnRight();
+      else if (dir == LEFT)
+        turnLeft();
+      break;
+    }
+  }
 }
 
 void turnRight() {
-
+  for (int i = 0; i < GO_STRAIGHT_BEFORE_TURN; i++) {
+    roll(FOWARD, SPEED, TICK);
+    delay(TOCK);
+  }
+  
+  while(1) {
+    sense();
+    roll(RIGHT, TURNSPEED, TURNTIME);
+    delay(TOCK);
+    
+    if (sensorStatus == SENSOR_F)
+      break;
+  }
 }
 
 void turnLeft() {
-
+  for (int i = 0; i < GO_STRAIGHT_BEFORE_TURN; i++) {
+    roll(FOWARD, SPEED, TICK);
+    delay(TOCK);
+  }
+  
+  int previous = sensorStatus;
+  
+  while(1) {
+    sense();
+    roll(LEFT, TURNSPEED, TURNTIME);
+    delay(TOCK);
+    
+    if (!(previous & SENSOR_F) && (sensorStatus & SENSOR_F)) {
+      break;
+    }
+    previous = sensorStatus;
+  }
 }
 
+//오른쪽으로 돌다가 길을 발견하면 그 길로 간다 (가장 왼쪽으로 가는 방법)
 void turnAround() {
-	roll(RIGHT, TURNSPEED, TURNTIME);
-
-	do {
-		sense()
-		roll(RIGHT, TURNSPEED, TICK);
-	} while (sensorStatus != SENSOR_F);
+  while(1) {
+    sense();
+    roll(RIGHT, TURNSPEED, TICK);
+    delay(TOCK);
+  
+    if (sensorStatus == SENSOR_F)
+      break;
+  }
 }
 
 void setup() {
-	pinMode(TRIGGER_PIN, OUTPUT);
-	pinMode(ECHO_PIN, INPUT);
+  Serial.begin(9600);
+  
+  pinMode(TRIGGER_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
 
-	pinMode(ENA, OUTPUT);
-	pinMode(IN1, OUTPUT);
-	pinMode(IN2, OUTPUT);
-	pinMode(ENB, OUTPUT);
-	pinMode(IN3, OUTPUT);
-	pinMode(IN4, OUTPUT);
+  pinMode(ENA, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(ENB, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
 
-	wheel(L_WHEEL | R_WHEEL, STOP, 0);
+  wheel(L_WHEEL | R_WHEEL, STOP, 0);
 }
 
 void loop() {
-	//감지 --> 판단 --> 움직임
-	sense();
+  //감지 --> 판단 --> 움직임
+  sense();
 
-	//앞에 장애물이 있는지 체크하고
-	if (distance < DISTANCE_THRESHOLD) {
-		distanceCount++;
-	} else {
-		ditanceCount = 0;
-	}
-	//장애물이 있다면(THRESHOLD회 이상 있는 것으로 판단된 경우)뒤로 돈다.
-	if (distanceCount < DISTANCE_COUNT_THRESHOLD) {
-		distanceCount = 0;
-		turnAround();
-	
-		return;
-	}
+  //앞에 장애물이 있는지 체크하고
+  if (distance < DISTANCE_THRESHOLD) {
+    distanceCount++;
+  } else {
+    distanceCount = 0;
+  }
+  //장애물이 있다면(THRESHOLD회 이상 있는 것으로 판단된 경우)뒤로 돈다.
+  if (distanceCount >= DISTANCE_COUNT_THRESHOLD) {
+    distanceCount = 0;
+    turnAround();
+    return;
+  }
+  
+  //사거리를 만난 경우
+  if (sensorStatus == (SENSOR_F | SENSOR_R | SENSOR_L)) {
+    turnLeft();
+    return;
+  } 
+  //오른쪽 갈림길만 있는 경우
+  else if (sensorStatus == (SENSOR_R | SENSOR_F)) {
+    //roll(FOWARD, TURNSPEED, TICK/2);
+    goStraight(RIGHT);
+    return;
+  } 
+  //왼쪽 갈림길만 있는 경우
+  else if (sensorStatus == (SENSOR_L | SENSOR_F)) {
+    goStraight(LEFT);
+    turnLeft();
+    return;
+  }
+  
+  //직진
+  if (sensorStatus & SENSOR_L) {
+    //roll(FOWARD, TURNSPEED, TICK/2);
+    roll(LEFT, TURNSPEED, TICK);
+    delay(TOCK);
+  } else if (sensorStatus & SENSOR_R) {
+    //roll(FOWARD, TURNSPEED, TICK/2);
+    roll(RIGHT, TURNSPEED, TICK);
+    delay(TOCK);
+  } else if (sensorStatus & SENSOR_F) {
+    roll(FOWARD, SPEED, TICK);
+    delay(TOCK);
+  }
 
-	//사거리를 만난 경우
-	if (sensorStatus & SENSOR_F && sensorStatus & SENSOR_R && sensorStatus & SENSOR_L) {
-		turnRight();
-
-		return;
-	}
-
-	//직진
-	if (sensorStatus & SENSOR_L) {
-		roll(RIGHT, SPEED, TICK);
-		delay(TOCK);
-	} else if (sensorStatus & SENSOR_R) {
-		roll(LEFT, SPEED, TICK);
-		delay(TOCK);
-	} else {
-		roll(FRONT, SPEED, TICK);
-		delay(TOCK);
-	}
-	//모든 작업이 끝난 후 약간 멈추기
-	delay(0);	
+  //길을 이탈한 경우
+  if (sensorStatus == 0) {
+    roll(FOWARD, SPEED, TICK);
+    delay(TOCK);
+  }
 }
